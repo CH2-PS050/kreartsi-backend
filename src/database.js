c290ba1686efb52a0cfbb7ca024f894b5e7b5c27
Claudia <88203931+clauculus@users.noreply.github.com>
2023-12-06 10:24:39 +0700

    update

--- src/database.js
+++ src/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-  user: 'root',
-  password: 'KreartsiBangkit2023',
-  database: 'kreartsidb',
-  host: '127.0.0.1',
-  port: '3306'
+  user: '',
+  password: '',
+  database: '',
+  host: '',
+  port: ''
 });
 
 module.exports = pool;
