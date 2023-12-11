d0696111f9e5ee43080250606d6bb4857211a608
Claudia <88203931+clauculus@users.noreply.github.com>
2023-12-12 00:10:33 +0700

    chore: remove try catch

--- src/controllers/art.controller.js
+++ src/controllers/art.controller.js
@@ -1,182 +1,183 @@
-const pool = require('../database');
+const pool = require("../database");
 
 // Get All Arts
 exports.getArts = async (req, res) => {
-  try {
-    // masi ngasal but it works
-    pool.query('SELECT * FROM Artworks', (error, results) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-      } else {
-        res.json(results);
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  pool.query("SELECT * FROM Artworks", (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.json(results);
+    }
+  });
 };
 
-// Get My Arts 
+// Get My Arts
 exports.getMyArts = async (req, res) => {
-  try {
-    const user_id = res.locals.user.user_id;
+  const user_id = res.locals.user.user_id;
 
-    pool.query('SELECT * FROM Artworks WHERE user_id = ?', [user_id], (error, results) => {
+  pool.query(
+    "SELECT * FROM Artworks WHERE user_id = ?",
+    [user_id],
+    (error, results) => {
       if (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send("Internal Server Error");
       } else {
         res.status(200).json(results);
       }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    }
+  );
 };
 
 // Get Art by ID
 exports.getArtById = async (req, res) => {
-  try {
-    const { artwork_id } = req.params;
+  const { artwork_id } = req.params;
 
-    pool.query('SELECT * FROM Artworks WHERE artwork_id = ?', [artwork_id], (error, results) => {
+  pool.query(
+    "SELECT * FROM Artworks WHERE artwork_id = ?",
+    [artwork_id],
+    (error, results) => {
       if (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send("Internal Server Error");
       } else {
         if (results.length === 0) {
-          return res.status(400).json({ msg: 'Artwork not found' });
+          return res.status(400).json({ msg: "Artwork not found" });
         } else {
           res.status(200).json(results);
         }
       }
-    });
-  } catch (error) {
-    console.log(error);
-  }
+    }
+  );
 };
 
 // Upload Art
 exports.uploadArt = async (req, res) => {
-  try {
-    const { caption, image_url, category_id } = req.body;
-    const user_id = res.locals.user.user_id;
+  const { caption, image_url, category_id } = req.body;
+  const user_id = res.locals.user.user_id;
 
-    if (!caption || !image_url || !category_id ) {
-      return res.status(400).json({ msg: "Please enter all fields" });
-    }
+  if (!caption || !image_url || !category_id) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
 
-    // PANGGIL MODEL ML BUAT DETECT CATEGORY
-    const newArtQuery = 'INSERT INTO Artworks (caption, image_url, category_id, user_id) VALUES (?, ?, ?, ?)';
-    pool.query(newArtQuery, [caption, image_url, category_id, user_id], (error, results) => {
+  // PANGGIL MODEL ML BUAT DETECT CATEGORY
+  const newArtQuery =
+    "INSERT INTO Artworks (caption, image_url, category_id, user_id) VALUES (?, ?, ?, ?)";
+  pool.query(
+    newArtQuery,
+    [caption, image_url, category_id, user_id],
+    (error, results) => {
       if (error) {
         console.error(error);
-        return res.status(500).send('Internal Server Error');
+        return res.status(500).send("Internal Server Error");
       } else {
         let response = {
           id: results.insertId,
           caption: caption,
           image_url: image_url,
           category_id: category_id,
-          user_id: user_id
-        }
-        res.status(200).json({ msg: "Artwork uploaded successfully", art: response })
+          user_id: user_id,
+        };
+        res
+          .status(200)
+          .json({ msg: "Artwork uploaded successfully", art: response });
       }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
+    }
+  );
+};
 
 // DONATE
 exports.donation = async (req, res) => {
-  try {
-    const { donated_amount } = req.body;
-    const recipient_user_id = req.params.user_id;
-    const donor_user_id = res.locals.user.user_id;
+  const { donated_amount } = req.body;
+  const recipient_user_id = req.params.user_id;
+  const donor_user_id = res.locals.user.user_id;
 
-    if (!donated_amount) {
-      return res.status(400).json({ msg: "Please input the amount" });
-    }
+  if (!donated_amount) {
+    return res.status(400).json({ msg: "Please input the amount" });
+  }
 
-    const newDonationQuery = 'INSERT INTO Donations (donated_amount, donor_user_id, recipient_user_id) VALUES (?, ?, ?)';
-    pool.query(newDonationQuery, [donated_amount, donor_user_id, recipient_user_id], (error, results) => {
+  const newDonationQuery =
+    "INSERT INTO Donations (donated_amount, donor_user_id, recipient_user_id) VALUES (?, ?, ?)";
+  pool.query(
+    newDonationQuery,
+    [donated_amount, donor_user_id, recipient_user_id],
+    (error, results) => {
       if (error) {
         console.error(error);
-        return res.status(500).send('Internal Server Error');
+        return res.status(500).send("Internal Server Error");
       } else {
-
         try {
           // add coin
-          const updateRecipientCoinQuery = 'UPDATE Users SET coins = coins + ? WHERE user_id = ?';
-          pool.query(updateRecipientCoinQuery, [donated_amount, recipient_user_id], (error, results) => {
-            if (error) {
-              console.error(error);
-              return res.status(500).send('Internal Server Error');
+          const updateRecipientCoinQuery =
+            "UPDATE Users SET coins = coins + ? WHERE user_id = ?";
+          pool.query(
+            updateRecipientCoinQuery,
+            [donated_amount, recipient_user_id],
+            (error, results) => {
+              if (error) {
+                console.error(error);
+                return res.status(500).send("Internal Server Error");
+              }
             }
-          });
+          );
           // deduct coin
-          const updateDonorCoinQuery = 'UPDATE Users SET coins = coins - ? WHERE user_id = ?';
-          pool.query(updateDonorCoinQuery, [donated_amount, donor_user_id], (error, results) => {
-            if (error) {
-              console.error(error);
-              return res.status(500).send('Internal Server Error');
+          const updateDonorCoinQuery =
+            "UPDATE Users SET coins = coins - ? WHERE user_id = ?";
+          pool.query(
+            updateDonorCoinQuery,
+            [donated_amount, donor_user_id],
+            (error, results) => {
+              if (error) {
+                console.error(error);
+                return res.status(500).send("Internal Server Error");
+              }
             }
-          });
+          );
 
           let response = {
             id: results.insertId,
             donated_amount: donated_amount,
             donor_user_id: donor_user_id,
-            recipient_user_id: recipient_user_id
-          }
-          res.status(200).json({ msg: "Donation success", donation: response })
-
+            recipient_user_id: recipient_user_id,
+          };
+          res.status(200).json({ msg: "Donation success", donation: response });
         } catch (error) {
           console.log(error);
         }
       }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
+    }
+  );
+};
 
-// Like Art 
+// Like Art
 exports.likeArt = async (req, res) => {
-  try {
-    const { artwork_id } = req.params;
+  const { artwork_id } = req.params;
 
-    const likeArtQuery = 'UPDATE Artworks SET likes_count = likes_count + 1 WHERE artwork_id = ?';
-    pool.query(likeArtQuery, [artwork_id], (error, results) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).send('Internal Server Error');
-      } else {
-        res.status(200).json({ msg: "Artwork liked" })
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
+  const likeArtQuery =
+    "UPDATE Artworks SET likes_count = likes_count + 1 WHERE artwork_id = ?";
+  pool.query(likeArtQuery, [artwork_id], (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send("Internal Server Error");
+    } else {
+      res.status(200).json({ msg: "Artwork liked" });
+    }
+  });
+};
 
-// Unlike 
+// Unlike
 exports.unlikeArt = async (req, res) => {
-  try {
-    const { artwork_id } = req.params;
+  const { artwork_id } = req.params;
 
-    const unlikeArtQuery = 'UPDATE Artworks SET likes_count = likes_count - 1 WHERE artwork_id = ?';
-    pool.query(unlikeArtQuery, [artwork_id], (error, results) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).send('Internal Server Error');
-      } else {
-        res.status(200).json({ msg: "Artwork unliked" })
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
+  const unlikeArtQuery =
+    "UPDATE Artworks SET likes_count = likes_count - 1 WHERE artwork_id = ?";
+  pool.query(unlikeArtQuery, [artwork_id], (error, results) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send("Internal Server Error");
+    } else {
+      res.status(200).json({ msg: "Artwork unliked" });
+    }
+  });
+};
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,119 +1,112 @@
-const { hashPassword, comparePassword } = require('../helpers/bcrypt');
-const { generateToken } = require('../helpers/jwt');
-const pool = require('../database');
+const { hashPassword, comparePassword } = require("../helpers/bcrypt");
+const { generateToken } = require("../helpers/jwt");
+const pool = require("../database");
 
 // sebenarnya will be better kalo pake sequelize ORM tp error wkt aku coba jd sementara query" gini aja dulu
 // Get All Users
 exports.getUsers = async (req, res) => {
-  try {
-
-    pool.query('SELECT * FROM Users', (error, results) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
-      } else {
-        res.json(results);
-      }
+  pool.query("SELECT * FROM Users", (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+    } else {
+      res.json(results);
     }
-    );
-  } catch (error) {
-    console.log(error);
-  }
+  });
 };
 
 // Get User by ID
 exports.getUserById = async (req, res) => {
-  try {
-    const { user_id } = req.params;
+  const { user_id } = req.params;
 
-    pool.query('SELECT * FROM Users WHERE user_id = ?', [user_id], (error, results) => {
+  pool.query(
+    "SELECT * FROM Users WHERE user_id = ?",
+    [user_id],
+    (error, results) => {
       if (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send("Internal Server Error");
       } else {
         if (results.length === 0) {
-          return res.status(400).json({ msg: 'User not found' });
+          return res.status(400).json({ msg: "User not found" });
         } else {
           res.status(200).json(results);
         }
       }
     }
-    );
-  } catch (error) {
-    console.log(error);
-  }
+  );
 };
 
 // Register User
 exports.registerUser = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (!username || !email || !password) {
-      return res.status(400).json({ msg: "Please enter all fields" });
+  console.log(username, email, password);
+  if (!username || !email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+  const existingUserQuery = "SELECT * FROM Users WHERE email = ?";
+  pool.query(existingUserQuery, [email], (error, results) => {
+    console.log(results);
+    if (results.length > 0) {
+      return res.status(400).json({ msg: "User already exists" });
+    } else if (error) {
+      console.error(error);
+      return res.status(500).send("Internal Server Error");
     }
 
-    const existingUserQuery = 'SELECT * FROM Users WHERE email = ?';
-    pool.query(existingUserQuery, [email], (error, results) => {
-      if (results.length > 0) {
-        return res.status(400).json({ msg: "User already exists" });
-      } else if (error) {
-        console.error(error);
-        return res.status(500).send('Internal Server Error');
-      }
+    const hashedPassword = hashPassword(password);
 
-      const hashedPassword = hashPassword(password);
-
-      pool.query(
-        'INSERT INTO Users (username, email, password) VALUES (?, ?, ?)',
-        [username, email, hashedPassword],
-        (insertError, insertResults) => {
-          if (insertError) {
-            console.error(insertError);
-            return res.status(500).send('Internal Server Error');
-          } else {
-            let response = {
-              id: insertResults.insertId,
-              username: username,
-              email: email
-            }
-            return res.status(200).json({ msg: "User registered successfully", user: response });
-          }
+    pool.query(
+      "INSERT INTO Users (username, email, password) VALUES (?, ?, ?)",
+      [username, email, hashedPassword],
+      (insertError, insertResults) => {
+        if (insertError) {
+          console.error(insertError);
+          return res.status(500).send("Internal Server Error");
+        } else {
+          let response = {
+            id: insertResults.insertId,
+            username: username,
+            email: email,
+          };
+          return res
+            .status(200)
+            .json({ msg: "User registered successfully", user: response });
         }
-      );
-    });
-
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send('Internal Server Error');
-  }
+      }
+    );
+  });
 };
-
 
 // Login User
 exports.loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ msg: "Please enter all fields" });
-    }
+  if (!email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
 
-    pool.query('SELECT * FROM Users WHERE email = ?', [email], (error, results) => {
+  pool.query(
+    "SELECT * FROM Users WHERE email = ?",
+    [email],
+    (error, results) => {
       if (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send("Internal Server Error");
       } else {
         if (results.length === 0) {
-          return res.status(400).json({ msg: 'User does not exists' });
+          return res.status(400).json({ msg: "User does not exists" });
         }
         const isMatch = comparePassword(password, results[0].password);
-        if(!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+        if (!isMatch)
+          return res.status(400).json({ msg: "Invalid credentials" });
 
         let payload = {
           id: results[0].id,
           email: results[0].email,
-          username: results[0].username
+          username: results[0].username,
         };
 
         const token = generateToken(payload);
@@ -123,13 +116,10 @@ exports.loginUser = async (req, res) => {
           user: {
             id: results[0].id,
             email: results[0].email,
-            username: results[0].username
-          }
+            username: results[0].username,
+          },
         });
       }
     }
-    );
-  } catch (error) {
-    console.log(error);
-  }
-}
+  );
+};
